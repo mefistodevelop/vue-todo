eaa8f41ae9341b6d97d6b5ec49915d8c9e7aad27
Oleg Rudenko <oleg.ru93@yandex.ru>
2020-12-06 14:58:52 +0300

    add custom styles to checkbox

--- src/components/common/CheckboxButton.vue
+++ src/components/common/CheckboxButton.vue
@@ -1,12 +1,15 @@
 <template>
   <div class="checkbox-button">
-    <label for="" class="label" />
-    <input
-      type="checkbox"
-      :checked="isChecked"
-      v-model="checked"
-      @change="$emit('mark-completed', checked)"
-    />
+    <label class="label">
+      <input
+        class="checkbox"
+        type="checkbox"
+        :checked="isChecked"
+        v-model="checked"
+        @change="$emit('mark-completed', checked)"
+      />
+      <div class="checkmark" />
+    </label>
   </div>
 </template>
 
@@ -41,6 +44,41 @@ export default {
   cursor: pointer;
 }
 
-/* .checkbox {
-} */
+.label::before {
+  content: '';
+  position: absolute;
+  top: -0.55rem;
+  left: -0.625rem;
+  width: 1.5rem;
+  height: 1.5rem;
+  border: 1px solid #757575;
+  border-radius: 4px;
+}
+
+.checkmark::before {
+  content: '\2713';
+  display: none;
+  position: absolute;
+  top: -0.85rem;
+  left: -0.625rem;
+  width: 1.5rem;
+  height: 1.5rem;
+  color: #00c853;
+  font-size: 1.5rem;
+  font-weight: 700;
+}
+
+.checkbox {
+  position: absolute;
+  top: 0;
+  left: 0;
+  width: 0;
+  height: 0;
+  opacity: 0;
+  cursor: pointer;
+}
+
+.checkbox:checked ~ .checkmark::before {
+  display: block;
+}
 </style>
