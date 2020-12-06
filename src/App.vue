3c563520fbb367ac7ffaf8deaa94136d4cd6d4c3
Oleg Rudenko <oleg.ru93@yandex.ru>
2020-12-06 12:08:20 +0300

    add TodoCard component

--- src/App.vue
+++ src/App.vue
@@ -1,23 +1,36 @@
 <template>
   <div id="app">
     <Header />
+    <main class="container">
+      <h1>TODO</h1>
+      <TodoCard />
+    </main>
   </div>
 </template>
 
 <script>
 import Header from "./components/Header";
+import TodoCard from "./components/TodoCard";
+
 export default {
   name: "App",
-  components: { Header },
+  components: { Header, TodoCard },
 };
 </script>
 
-<style>
+<style scoped>
 #app {
   font-family: "Montserrat", sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
+  height: 100%;
+}
+
+.container {
+  margin: 0 auto;
+  max-width: 1400px;
+  width: 100%;
 }
 </style>
---
+++ src/components/TodoCard.vue
@@ -0,0 +1,35 @@
+<template>
+  <div class="todo-card">
+    <input type="checkbox" />
+    <p class="todo-text">Lorem ipsum dolor sit amet consectetur.</p>
+    <b-button variant="danger">&times;</b-button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "TodoCard",
+};
+</script>
+
+<style scoped>
+.todo-card {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  padding: 0.5rem 2rem;
+  max-width: 50rem;
+  border: 1px solid #2c3e5066;
+  border-radius: 4px;
+}
+
+.todo-card:hover,
+.todo-card:focus {
+  box-shadow: 0 0 4px 1px #2c3e5066;
+}
+
+.todo-text {
+  margin: 0;
+  padding: 0;
+}
+</style>
